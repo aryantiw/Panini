c1766455408d8540fa7cf3325612584c009c71ed
Aryan Tiwari <77456196+aryantiw@users.noreply.github.com>
2023-06-05 21:50:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ print("नमस्कार जगत्")
 Bhai yrr kbhi kbhi to kuch krne ka mnn nhi krta hai jeevan ka Satya khojne nikla hu par kuch mil nhi rha hai koi safakta haasil nhi ho rhi kre to kre kya
 
 Mai fir aa gya faltu commit krne
+wo heat map bhara bhara dikhna chaiye na
