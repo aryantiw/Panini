f3b5f714af4eeef69274a63add3325cd57666e64
Aryan Tiwari <77456196+aryantiw@users.noreply.github.com>
2023-06-05 21:50:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("नमस्कार जगत्")
 Bhai yrr kbhi kbhi to kuch krne ka mnn nhi krta hai jeevan ka Satya khojne nikla hu par kuch mil nhi rha hai koi safakta haasil nhi ho rhi kre to kre kya
+
+Mai fir aa gya faltu commit krne
